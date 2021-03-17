f45c4f3012503a74bc4e77a431bfcf10f36bf150
kyc <kangycn@gmail.com>
2021-03-17 00:51:06 -0400

    update 20201005: use setPixelRatio to improve display quality on mobile devices

--- assets/20201005/cube.js
+++ assets/20201005/cube.js
@@ -26,6 +26,7 @@ function init() {
 
   // renderer
   renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
   container.appendChild(renderer.domElement);
 
